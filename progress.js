3db91ff04c9389846224126cb2c5b863184d8849
NjFaiaz <njfaiaz@gmail.com>
2023-06-21 21:09:42 +0600

    First Commit

---
+++ progress.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    let bar = document.querySelectorAll('.bar');
+    bar.forEach((progress) => {
+      let value = progress.getAttribute('data-value');
+      progress.style.width = `${value}%`;
+      let count = 0;
+      let progressAnimation = setInterval(() => {
+        count++;
+        progress.setAttribute('data-text', `${count}%`);
+        if (count >= value) {
+          clearInterval(progressAnimation);
+        }
+      }, 15);
+    });
+  };
